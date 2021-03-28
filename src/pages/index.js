6380f7dc1c98c720a29ad7540b89b4c7f4564da3
Thomas Wilthil <thomas.wilthil@ntb.no>
2021-03-28 15:28:48 +0200

    Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,12 @@
 import React from "react"
-
 import Layout from "../components/layout"
 import Hero from "../components/hero/Hero"
 import SEO from "../components/seo"
 
-
-
 const IndexPage = () => {
-
   return (
     <Layout>
       <SEO title="Stray Vyrje" />
-
       <Hero />
     </Layout>
   )
