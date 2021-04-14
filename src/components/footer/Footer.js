e00cdf192aedb44fb4acbaa289159fc7e74fd329
Thomas Wilthil <thomas.wilthil@ntb.no>
2021-04-14 16:29:00 +0200

    Add hidden field

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -20,6 +20,7 @@ const Footer = () => {
         </Section>
         <Section>
           <form name="contact" method="POST" data-netlify="true">
+            <input type="hidden" name="form-name" value="contact" />
             <p>
               <label>Your Name: <input type="text" name="name" /></label>
             </p>
